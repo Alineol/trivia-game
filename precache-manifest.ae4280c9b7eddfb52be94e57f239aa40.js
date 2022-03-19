96437efdab7d3430ba0839ac1733f582d9e8a0d4
Aline <allinne.oliveira.ol@gmail.com>
2022-03-19 11:50:20 -0300

    Updates

---
+++ precache-manifest.ae4280c9b7eddfb52be94e57f239aa40.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2153dd24272792cfa10a5426f3da3af",
+    "url": "/trivia-game/index.html"
+  },
+  {
+    "revision": "fc357ccdf2eecd0c84c8",
+    "url": "/trivia-game/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "96b6b53fd3384eae37cf",
+    "url": "/trivia-game/static/css/main.74c61f77.chunk.css"
+  },
+  {
+    "revision": "fc357ccdf2eecd0c84c8",
+    "url": "/trivia-game/static/js/2.cdc73f8a.chunk.js"
+  },
+  {
+    "revision": "6b427be26341f18e5beeadca3932038e",
+    "url": "/trivia-game/static/js/2.cdc73f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b6b53fd3384eae37cf",
+    "url": "/trivia-game/static/js/main.44e99bd6.chunk.js"
+  },
+  {
+    "revision": "b0c6c90c513f51408a18",
+    "url": "/trivia-game/static/js/runtime-main.4bc9de50.js"
+  },
+  {
+    "revision": "aa4403536948f206886c2c9066e14c96",
+    "url": "/trivia-game/static/media/bip.aa440353.mp3"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/trivia-game/static/media/trivia.466d153e.png"
+  }
+]);
